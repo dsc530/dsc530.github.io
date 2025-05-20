8fb08c697082d2500d8d57a456a94f5d68240ec4
Daniel Cumbal <66384040+dsc530@users.noreply.github.com>
2025-05-20 17:47:30 -0500

    content update

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -40,8 +40,8 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     website: 'https://example.com',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Daniel Cumbal DS',
+    subtitle: 'Welcome to my Data Science Experience',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -98,8 +98,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hi There & Welcome to My Journey with Data and ',
+        text: "I am a professional with a solid background in IT, With experience in technical support and automation. On these fields, I’ve acquired critical skills such as complex problem-solving, customer service, and project management. Over time, I discovered my passion for data analysis and its potential to transform business decision-making. My interest in data began while working with automated systems, where I saw how patterns in information could optimize processes. This realization led me to specialize in Data Science, a field that allows me to leverage my existing expertise while developing new predictive models and machine learning solutions. Feel free to explore some of my coding endeavors on <a href='https://github.com/dsc530>GitHub</a> or follow me on <a href='https://www.linkedin.com/in/daniel-cumbal/?locale=en_US'>Linkeind</a>.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
